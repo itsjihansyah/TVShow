42a8659003bb2f8faf20f8cd464aa8808848de33
Jihan Syahira <111284378+itsjihansyah@users.noreply.github.com>
2026-07-29 10:33:13 +0700

    Initial Commit

---
+++ TVShow/TVShowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVShowApp.swift
+//  TVShow
+//
+//  Created by Jihan Syahira Adnanda Putri on 29/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TVShowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
